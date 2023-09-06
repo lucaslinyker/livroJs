e24208706782003024a35f90dedb2aab0e34d052
lucaslinyker <lucaslinyker@outlook.com>
2023-09-06 15:56:15 -0300

    Criei ex4_2.js

---
+++ cap04/js/ex4_2.js
@@ -0,0 +1,22 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+  const nome = frm.inNome.value
+  const masculino = frm.inMasculino.checked
+  const altura = Number(frm.inAltura.value)
+
+  let peso
+  if (masculino) {
+    peso = 22 * Math.pow(altura, 2)
+  } else {
+    peso = 21 * (altura ** 2)
+  }
+  resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(3)} kg`
+
+  e.preventDefault()
+})
+
+frm.addEventListener("reset", () => {
+  resp.innerText = ''
+})
