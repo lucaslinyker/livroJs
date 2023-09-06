cc9fb000d4a03cb7cccecf1a0ff6cb380f21b674
lucaslinyker <lucaslinyker@outlook.com>
2023-09-06 16:14:47 -0300

    Troquei else if por ternário

--- cap04/js/ex4_2.js
+++ cap04/js/ex4_2.js
@@ -6,12 +6,8 @@ frm.addEventListener("submit", (e) => {
   const masculino = frm.inMasculino.checked
   const altura = Number(frm.inAltura.value)
 
-  let peso
-  if (masculino) {
-    peso = 22 * Math.pow(altura, 2)
-  } else {
-    peso = 21 * (altura ** 2)
-  }
+  const peso = masculino ? 22 * Math.pow(altura, 2) : 21 * (altura ** 2)
+
   resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(3)} kg`
 
   e.preventDefault()
