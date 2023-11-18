84018dc04cf33f7855b2b0dc7d26ce3b71fb2217
lucaslinyker <lucaslinyker@outlook.com>
2023-11-18 12:16:55 -0300

    Criei ex5_9.js

---
+++ cap05/nodejs/ex5_9.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')()
+console.clear()
+
+const produto = prompt('Produto: ')
+const num = Number(prompt('Nº de Etiquetas: '))
+
+for (let i = 1; i <= num / 2; i++) {
+    console.log(`${produto.padEnd(30)} ${produto.padEnd(30)}`);
+}
+if (num % 2 == 1) {
+    console.log(produto);
+}
